6d829f1657e203336f5bf489d542ed1f90b85f42
AmitAlloni <amitalloni123@gmail.com>
2024-07-04 14:23:20 +0300

    CoffeeShopActivity with sort and search

--- app/src/main/java/com/example/coffeecompass/activity/LoginActivity.kt
+++ app/src/main/java/com/example/coffeecompass/activity/LoginActivity.kt
@@ -2,8 +2,10 @@ package com.example.coffeecompass.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.coffeecompass.R
@@ -12,6 +14,7 @@ import com.google.firebase.auth.FirebaseAuth
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
+    private lateinit var progressBar: ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,17 +25,24 @@ class LoginActivity : AppCompatActivity() {
         val emailEditText = findViewById<EditText>(R.id.emailEditText)
         val passwordEditText = findViewById<EditText>(R.id.passwordEditText)
         val loginButton = findViewById<Button>(R.id.loginWithEmailButton)
+        progressBar = findViewById(R.id.progressBar)
 
         loginButton.setOnClickListener {
             val email = emailEditText.text.toString()
             val password = passwordEditText.text.toString()
-            loginUser(email, password)
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                loginUser(email, password)
+            } else {
+                Toast.makeText(this, "Please fill out all fields", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
     private fun loginUser(email: String, password: String) {
+        progressBar.visibility = View.VISIBLE
         auth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
+                progressBar.visibility = View.GONE
                 if (task.isSuccessful) {
                     // Login success, navigate to HomeActivity
                     val intent = Intent(this, HomeActivity::class.java)
@@ -40,7 +50,8 @@ class LoginActivity : AppCompatActivity() {
                     finish()
                 } else {
                     // Login failed, show error message
-                    Toast.makeText(this, "Authentication Failed", Toast.LENGTH_SHORT).show()
+                    val errorMessage = task.exception?.message ?: "Authentication Failed"
+                    Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show()
                 }
             }
     }
